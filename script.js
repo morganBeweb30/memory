e04c3bcb4a17d622b52a4c2ebd64999bbd0259ae
morganBeweb30 <morg.leen@ouvaton.org>
2019-11-18 06:59:13 +0100

    toDo : cacher cartes pour 2è partie

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 function placerCartes() {
-
+    document.getElementById('gagne').className='d-none';
+//    document.getElementById('table').className='d-block';
+    document.getElementById('overlay').className='heigth0';
     let cartes = ['1','1','2','2','3','3','4','4','5','5','6','6'];
     let ids = ['a0','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11']
    
@@ -8,14 +10,15 @@ function placerCartes() {
 
         let j = Math.floor(Math.random() * ids.length);
 
-        document.getElementById('msg').innerHTML+=j;       //affiche le tirage
+//        document.getElementById('msg').innerHTML+=j;       //affiche le tirage
         document.getElementById(ids[j]).innerHTML='<img id="img'+ids[j]+'" src="./captures/jimag'+cartes[i]+'.gif">';  // 
             //affiche la carte dans la case ids[j] pour vérif
         ids.splice(j,1);                                    //supprime la case remplie
-        document.getElementById('msg').innerHTML+=' '+JSON.stringify(ids)+'<br>';      //affiche les cases restatnes pour vérif
+//        document.getElementById('msg').innerHTML+=' '+JSON.stringify(ids)+'<br>';      //affiche les cases restatnes pour vérif
+        document.getElementById('nouvPartie').className='d-none';      // enlève bouton "nouvelle partie"
     }
 }
-placerCartes();
+// placerCartes();
 /*
 function test(value) {
     console.log(value); //afiche bonjour dans la console
@@ -28,59 +31,70 @@ let premCarte='';
 let deuzCarte='';
 let tds=document.querySelectorAll('td');
 let cartesTrouvees = 0;
+let cartesRestantes;
 
 function montrerUneCarte(value) {
-    document.getElementsByTagName('td')[value].className='affich vert';
+    document.getElementsByTagName('td')[value].className='affich';
     let imgIdValue='imga'+value;
     let tdIdValue='a'+value;
     
-    document.getElementById('msg').innerHTML=value;
+//    document.getElementById('msg').innerHTML=value;
 
     cpt+=1;
-    document.getElementById('msg').innerHTML+=premCase; // value de la case
+//    document.getElementById('msg').innerHTML+=premCase; // value de la case
     if(cpt%2!==0) {     // 1 carte retournée
         premCase=document.getElementById(tdIdValue);
-        premCarte=document.getElementById(imgIdValue).src; // chemin de l'image de la case cliquée dans variable premCarte
-        document.getElementById('msg').innerHTML+=' '+premCarte+' cliquer sur une autre carte';
-        document.getElementById('msg').innerHTML+=' case : '+premCase+'<br>';
+        premCarte=document.getElementById(imgIdValue).src; // chemin de l'image de la case cliquée dans variable deuzCarte
+        document.getElementById('msg').innerHTML='Cliquer sur une autre carte';
+//        document.getElementById('msg').innerHTML+=' '+premCarte+' cliquer sur une autre carte';
+//        document.getElementById('msg').innerHTML+=' case : '+premCase+'<br>';
     } else {
         deuzCase=document.getElementById(tdIdValue);
         deuzCarte=document.getElementById(imgIdValue).src; // chemin de l'image de la case cliquée dans variable deuzCarte
-        document.getElementById('msg').innerHTML+=' '+deuzCarte+' tester les 2 cartes';
-        document.getElementById('msg').innerHTML+=' case : '+deuzCase+'<br>';
+        document.getElementById('msg').innerHTML='Test des 2 cartes';
+//        document.getElementById('msg').innerHTML+=' '+deuzCarte+' tester les 2 cartes';
+//        document.getElementById('msg').innerHTML+=' case : '+deuzCase+'<br>';
+        document.getElementById('overlay').className='height400';   // interdire clic sur tableau
 
         // si pas les mêmes : les recacher
         if(premCarte !== deuzCarte) {
-            document.getElementById('msg').innerHTML+='<button id="cacherCartes" onclick="cacherCartes()">Cacher</button>';
-            document.getElementById('msg').innerHTML+=' '+premCarte+' != '+deuzCarte+' les 2 cartes sont !=';
+            document.getElementById('cacherCartes').className='d-block';
+            document.getElementById('supprCartes').className='d-none';
+//            document.getElementById('msg').innerHTML+=' '+premCarte+' != '+deuzCarte+' les 2 cartes sont !=';
         } else {    //si les mêmes : les supprimer, ajouter 2 à la variable (array) cartesTrouvees, et vérif si array cartesTrouvees.length === 12.
-            document.getElementById('msg').innerHTML+='<button id="supprCartes" onclick="supprCartes()">Enlever</button>';
-            document.getElementById('msg').innerHTML+=' '+premCarte+' = '+deuzCarte+' les 2 cartes sont =';
+            document.getElementById('supprCartes').className='d-block';
+            document.getElementById('cacherCartes').className='d-none';
+//            document.getElementById('msg').innerHTML+=' '+premCarte+' = '+deuzCarte+' les 2 cartes sont =';
         }
-        
-            // si cartesTrouvees.length !== 12 : afficher "cliquer sur une carte"
-            // si cartesTrouvees.length === 12 : afficher "gagné"
     }
 }
 
 function cacherCartes() {
     premCase.className='cases vert';
     deuzCase.className='cases vert';
+    document.getElementById('overlay').className='height0';    // autoriser clic sur tableau
+    document.getElementById('cacherCartes').className='d-none';
+    document.getElementById('msg').innerHTML=' ';
 }
 
 function supprCartes() {
     cartesTrouvees+=2;
+    cartesRestantes=12-cartesTrouvees;
     premCase.className='finie';
     premCase.innerHTML='<img src="./captures/jimag00.png">';
     deuzCase.className='finie';
     deuzCase.innerHTML='<img src="./captures/jimag00.png">';
+    document.getElementById('msg').innerHTML=' ';
+    document.getElementById('overlay').className='height0';    // autoriser clic sur tableau
     // Vérifier si il reste des cartes sur le plateau (casesSupprimees==12) :
     if(cartesTrouvees==12) {
         // afficher "partie gagnée"
-        document.getElementById('msg').innerHTML='Partie gagnée, Bravo !<br> :)';
-//        document.getElementById('msg').innerHTML+='<button id="nouvPartie" onclick="placerCartes();">Nouvelle partie</button>';
+        document.getElementById('gagne').className='d-block';
+        document.getElementById('table').className='d-none';
+        document.getElementById('supprCartes').className='d-none';
+//        document.getElementById('nouvPartie').className='vert';
     } else {
-        document.getElementById('msg').innerHTML='cases supprimées : '+cartesTrouvees;
+        document.getElementById('msg').innerHTML='cases restantes : '+cartesRestantes;
     }
 }
 
@@ -90,7 +104,23 @@ function supprCartes() {
 
 
 
+/***************** 
+<button id="myBtn">Try it</button>
 
+function montrerUneCarte(value,event) {
+  alert ("Hello World!");
+  event.stopImmediatePropagation(); // Try to remove me
+}
+
+function someOtherFunction() {
+  alert ("I will not get to say Hello World");
+}
+
+let x = document.getElementsByTagName("td");
+x.addEventListener("click", myFunction);
+x.addEventListener("click", someOtherFunction);
+
+******************/
 
 /*
 function randomNumberBetween(min, max) {
